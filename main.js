a765af49147e152685d380964ba14a71cba67dac
limzykenneth <limzy.kenneth@gmail.com>
2015-09-23 16:03:29 +0100

    Add description and minor responsive fix

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ function setup(){
 
 	canvas = createCanvas(w, h);
 	canvas.addClass('sketch');
-	// canvas.position((windowWidth - w)/2 , 20);
 
 	rectMode(CENTER);
 	stroke(0);
@@ -127,3 +126,14 @@ function keyPressed(){
 		loop();
 	}
 }
+
+function mouseClicked(){
+	loop();
+}
+
+$(document).ready(function() {
+	var description = 'From [Wikipedia] (https://en.wikipedia.org/wiki/Langton%27s_ant): &#8220;Langton&#8217;s ant is a two-dimensional Turing machine with a very simple set of rules but complex emergent behavior. It was invented by Chris Langton in 1986 and runs on a square lattice of black and white cells.&#8221; I recommend [this video] (https://www.youtube.com/watch?v=NWBToaXK5T0) by Numberphile to explain it better. --- Rules are simple: 1. At a white square, turn 90&#176; right, flip the color of the square, move forward one unit. | 2. At a black square, turn 90&#176; left, flip the color of the square, move forward one unit |';
+	var result = smark.generate(description).html;
+
+	$("#description").html(result);
+});
